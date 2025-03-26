a3caf715fbd10d80edcc222b94906ac4c0da55a0
anit <anit7795@gmail.com>
2025-03-26 20:44:41 +0530

    make delivcery options interactive

--- javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateCartQuantity, updateQuantity } from '../data/cart.js';
+import { cart, removeFromCart, calculateCartQuantity, updateQuantity,  } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -76,7 +76,8 @@ function deliveryOptionHTML(matchingProduct, cartItem) {
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
     
     html += `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option" data-product-id = "${matchingProduct.id}"
+      data-delivery-option-id= "${deliveryOption.id}">
         <input type="radio" ${isChecked ? 'checked' : ''} class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
         <div>
           <div class="delivery-option-date">${dateString}</div>
@@ -141,3 +142,11 @@ function updateCartQuantity() {
 }
 
 updateCartQuantity();
+
+document.querySelectorAll('.js-delivery-option')
+  .forEach((element) => {
+    element.addEventListener('click', () => {
+      const {productId,} = element.dataset
+      updateDeliveryOptions(productId, deliveryOptionId)
+    })
+  })
