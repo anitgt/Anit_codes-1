2f79df2a00ea21e1570ccb53e30de0c317559e9b
anit <anit7795@gmail.com>
2025-03-30 00:49:11 +0530

    regenerate HTML when updating delivery Option

--- javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateCartQuantity, updateQuantity,  } from '../data/cart.js';
+import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -11,6 +11,9 @@ const today = dayjs();
 
 const deliveryDate = today.add(7, 'days');
 console.log(deliveryDate.format('dddd, MMMM D'));
+
+function renderOrderSummary() {
+
 
 let cartSummaryHTML = '';
 
@@ -146,7 +149,11 @@ updateCartQuantity();
 document.querySelectorAll('.js-delivery-option')
   .forEach((element) => {
     element.addEventListener('click', () => {
-      const {productId,} = element.dataset
-      updateDeliveryOptions(productId, deliveryOptionId)
+      const {productId, deliveryOptionId} = element.dataset
+      updateDeliveryOption(productId, deliveryOptionId);
+      renderOrderSummary();
     })
   })
+}
+
+renderOrderSummary();
