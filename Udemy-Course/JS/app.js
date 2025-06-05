872108e493390e45be58e201f20e86896074bc85
anit <anit7795@gmail.com>
2025-06-05 22:22:38 +0530

    conditional

---
+++ Udemy-Course/JS/app.js
@@ -0,0 +1,14 @@
+console.log("before conditional");
+let ran = Math.random();
+
+if(ran < 0.5) {
+    console.log('Your numvber is less than 0.5');
+    console.log(ran)
+};
+
+console.log("After conditional");
+
+if(ran >= 0.5) {
+    console.log("your number is greater than 0.5");
+    console.log(ran);
+}
