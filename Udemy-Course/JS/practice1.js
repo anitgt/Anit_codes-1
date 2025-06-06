b5c67fb3019d831277fc12d1e6173e8707bee2e1
anit <anit7795@gmail.com>
2025-06-06 10:09:03 +0530

    And operator

--- Udemy-Course/JS/practice1.js
+++ Udemy-Course/JS/practice1.js
@@ -1,13 +1,21 @@
-const userInput = prompt("Enter a message: ")
+/* const userInput = prompt("Enter a message: ")
 
 if(userInput) {
     console.log("truthy")
 } else {
     console.log("falsy")
-}
+} */
 
 /*     if(0) {
         console.log("truthy")
     } else {
         console.log("falsy")
     } */
+
+const password = prompt("Enter your password: ");
+
+if(password.length >= 6 && password.indexOf(' ') === -1) {
+    console.log("Valid pass")
+} else {
+    console.log("Incorrect passwrod")
+}
