90c1ae64c92d10b4813ddc850b1d958adf81d8fc
anit <anit7795@gmail.com>
2025-06-06 23:18:41 +0530

    Arrays

---
+++ Udemy-Course/JS/arrays.js
@@ -0,0 +1,56 @@
+/* let days = ['monday', 'tuesday', 'wednesday']
+
+console.log(days);
+
+console.log(typeof(days));
+
+let kitchenSink = [true, undefined, "hello", 89]
+
+kitchenSink[0] = false;
+
+console.log(kitchenSink[2][1]);
+
+kitchenSink[90] = "hellowwwwww";
+
+kitchenSink.push("hehehe");
+kitchenSink.pop();
+
+kitchenSink.shift();
+kitchenSink.unshift('dayumm');
+
+console.log(kitchenSink);
+
+*/
+
+/* let cats = ['blue', 'kitty'];
+
+let dogs = ['monu', 'sonu'];
+
+console.log(cats.concat(dogs));
+
+console.log(cats.includes("blue"));
+
+console.log(cats.indexOf('kitty'));
+
+cats.reverse();
+console.log(cats);
+ */
+
+/* let colors = ['red', 'orange', 'green', 'blue', 'dark']; */
+
+/* let coolColors = colors.slice(3,5);
+
+let warmColors = colors.slice(1); */
+/*  */
+/* console.log(coolColors); */
+/* console.log(warmColors); */
+
+/* let splicing = colors.splice(2, 0,'red-orange');
+
+console.log(splicing)
+console.log(colors) */
+
+let scores = [0, 34, -33, 345, 788];
+
+console.log(scores.sort());
+
