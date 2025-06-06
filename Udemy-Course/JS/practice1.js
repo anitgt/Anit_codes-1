94e45ffd0e63ed5373749a4a850d1167a4539c24
anit <anit7795@gmail.com>
2025-06-06 10:53:42 +0530

    switch statements

--- Udemy-Course/JS/practice1.js
+++ Udemy-Course/JS/practice1.js
@@ -39,8 +39,37 @@ if(age> 0 && age <5 || age >= 65) {
     firstname = prompt("try again");
 } */
 
-const age = 8;
+/* const age = 8;
 
 if(!(age >= 0 && age <5 || age>=65)) {
     console.log("you are not a baby or a senior")
+} */
+
+
+
+const day = 8;
+
+switch(day) {
+    case 1:
+        console.log("monday");
+        break
+    case 2: 
+        console.log("tuesday");
+        break
+    case 3:
+        console.log("wednesday");
+        break
+    case 4:
+        console.log("Thursday");
+        break
+    case 5:
+        console.log("Friday");
+        break
+    case 6:
+    case 7:
+        console.log("weekend");
+        break
+    
+    default:
+        console.log("i dont know")
 }
