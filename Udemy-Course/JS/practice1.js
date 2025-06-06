15f7b4da436be47bcd31fec73f0faab580e51ab0
anit <anit7795@gmail.com>
2025-06-06 10:30:11 +0530

    or operator

--- Udemy-Course/JS/practice1.js
+++ Udemy-Course/JS/practice1.js
@@ -12,10 +12,23 @@ if(userInput) {
         console.log("falsy")
     } */
 
-const password = prompt("Enter your password: ");
+/* const password = prompt("Enter your password: ");
 
 if(password.length >= 6 && password.indexOf(' ') === -1) {
     console.log("Valid pass")
 } else {
     console.log("Incorrect passwrod")
+} */
+
+const age = -100;
+
+
+if(age> 0 && age <5 || age >= 65) {
+    console.log('free')
+} else if(age>=5 && age <10) {
+    console.log("10$")
+} else if(age>=10 && age <65) {
+    console.log("20$")
+} else {
+    console.log("invalid")
 }
