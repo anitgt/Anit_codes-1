d7e0d6d6fbca0f86d3df65f16068c89b7b81a3b1
anit <anit7795@gmail.com>
2025-06-07 22:02:38 +0530

    objects

---
+++ Udemy-Course/JS/object.js
@@ -0,0 +1,55 @@
+/* const person = { */
+/*     firstName: 'Mike', */
+/*     lastName: 'Jagger' */
+/* } */
+/*  */
+/* console.log(person); */
+/*  */
+/* const kitchenSink = { */
+/*     favNum: 90798, */
+/*     isFunny: 'true', */
+/*     colors: ['red', 'orange'] */
+/* } */
+/*  */
+/* console.log(kitchenSink) */
+/*  */
+
+/* const person = {
+    firstName: 'Mike',
+    lastName: 'Jagger'
+}
+
+console.log(person['lastName']);
+console.log(person.lastName); */
+
+/* const midTerms = {
+    thomas: 78,
+    elly: 99
+}
+
+midTerms.thomas = 89;
+
+midTerms['elly'] = 95;
+
+midTerms.ezra = 77;
+
+midTerms['Antonio'] = 'a-'
+
+console.log(midTerms); */
+
+
+const comments = [
+    {
+        username: 'tammy',
+        text: 'lololol',
+        votes: 9
+    },
+
+    {
+        username: "fishboi",
+        text: 'glub glub',
+        votes: 898
+    }
+]
+
+console.log(comments[1].text)
