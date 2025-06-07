0e5eea05f5c3d560ab22fadfb9d476555d854ef1
anit <anit7795@gmail.com>
2025-06-07 22:44:05 +0530

    loops basics

---
+++ Udemy-Course/JS/loops.js
@@ -0,0 +1,18 @@
+/* for(let i = 1; i <=10 ; i++) { */
+/*     console.log('i am in the loop body'); */
+/*     console.log(i); */
+/* } */
+
+
+/* for(let i = 1; i <= 20; i += 2 ) { */
+/*     console.log(i) */
+/* } */
+
+/* for(let i = 10; i <= 1000; i = i * 10) { */
+/*     console.log(i) */
+/* } */
+
+/* for(let i = 25;i>=0; i= i -5) {
+    console.log(i)
+} */
+
