d283c6f560e5fc1b621666c4ad2567b513c5342a
anit <anit7795@gmail.com>
2025-06-15 02:25:39 +0530

    return statement

--- Udemy-Course/JS/functions.js
+++ Udemy-Course/JS/functions.js
@@ -8,19 +8,29 @@
  /* singSong() */
 
 
- function greet(firstName, lastName) {
-    console.log(`Hello ${firstName} ${lastName[0]}.`)
- }
+/*  function greet(firstName, lastName) { */
+/*     console.log(`Hello ${firstName} ${lastName[0]}.`) */
+/*  } */
+/*  */
+/*  greet('Anit', 'Sarkar') */
+/*  */
+/*  */
+/*  function repeat(str, times) { */
+/*     let result = '' */
+/*     for(let i = 0; i < times; i++) { */
+/*         result += str; */
+/*     } */
+/*     console.log(result) */
+/*  } */
+/*  */
+/*  repeat('he ', 3) */
 
- greet('Anit', 'Sarkar')
-
-
- function repeat(str, times) {
-    let result = ''
-    for(let i = 0; i < times; i++) {
-        result += str;
+function add(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        return false;
     }
-    console.log(result)
- }
+    const sum = x + y;
+    return sum;
+}
 
- repeat('he ', 3)
+console.log(add('a', 20))
