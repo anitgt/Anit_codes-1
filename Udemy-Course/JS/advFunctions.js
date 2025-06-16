1d8febb8a291b3ceae62fef4c5a3757fbe4aae97
anit <anit7795@gmail.com>
2025-06-16 22:28:04 +0530

    Scope

---
+++ Udemy-Course/JS/advFunctions.js
@@ -0,0 +1,21 @@
+/* let totalEggs = 0; */
+/*  */
+/* function collectEggs() { */
+/*     totalEggs = 6; */
+/* } */
+/* console.log(totalEggs) */
+/* collectEggs() */
+/* console.log(totalEggs) */
+/*  */
+/*  */
+/*  */
+
+let bird = 'Scarlet';
+
+function birdWatch() {
+    /* let bird = 'Great blue'; */
+    console.log(bird)
+}
+
+birdWatch()
+console.log(bird);
