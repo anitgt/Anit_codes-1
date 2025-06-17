07c22cf55484bffb5bc0c33b3c9f80ea4f3d2a73
anit <anit7795@gmail.com>
2025-06-18 03:06:58 +0530

    This keyword and window

--- Udemy-Course/JS/methods.js
+++ Udemy-Course/JS/methods.js
@@ -8,6 +8,21 @@ const myMath = {
     }
 }
 
-console.log(myMath.cube(389))
+console.log(myMath.cube(389));
 
+const cat = {
+    name: 'Blue steele',
+    color: 'grey',
+    breed: "scottish fold",
+    meow() {
+        console.log(`${this.name} says meow`)
+        }
+}
 
+const cat2 = cat.meow
+
+console.log(cat);
+
+cat.meow()
+
+cat2()
