18bae4fa2b22f1b9adb6b5e4173376a59ced7e36
anit <anit7795@gmail.com>
2025-06-18 03:37:59 +0530

    For each method

--- Udemy-Course/JS/methods.js
+++ Udemy-Course/JS/methods.js
@@ -1,28 +1,36 @@
-const myMath = {
-    PI: 3.141,
-    square(num) {
-        return num * num;
-    },
-    cube(num) {
-        return num ** 3;
-    }
-}
+// const myMath = {
+    // PI: 3.141,
+    // square(num) {
+        // return num * num;
+    // },
+    // cube(num) {
+        // return num ** 3;
+    // }
+// }
+// 
+// console.log(myMath.cube(389));
+// 
+// const cat = {
+    // name: 'Blue steele',
+    // color: 'grey',
+    // breed: "scottish fold",
+    // meow() {
+        // console.log(`${this.name} says meow`)
+        // }
+// }
+// 
+// const cat2 = cat.meow
+// 
+// console.log(cat);
+// 
+// cat.meow()
+// 
+// cat2()
 
-console.log(myMath.cube(389));
+const numbers = [1,3,45,67,8,,5,5,5,345345,5,345,345,34,5,534,5,5];
 
-const cat = {
-    name: 'Blue steele',
-    color: 'grey',
-    breed: "scottish fold",
-    meow() {
-        console.log(`${this.name} says meow`)
-        }
-}
-
-const cat2 = cat.meow
-
-console.log(cat);
-
-cat.meow()
-
-cat2()
+numbers.forEach(function(el) {
+    if(el % 2 === 0) {
+        console.log(el);
+    } 
+})
