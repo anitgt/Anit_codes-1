097ea8fa2a51e7dc724c4d99519ac44213f26b6d
anit <anit7795@gmail.com>
2025-06-18 12:16:42 +0530

    Reduce method

--- Udemy-Course/JS/advFunctions.js
+++ Udemy-Course/JS/advFunctions.js
@@ -238,3 +238,40 @@ exams.every(score => score > 40);
 exams.some(score => score > 40)
 
 movies.some(m => m.year > 1990)
+
+const prices = [9.99, 1.50, 19.99, 49.99, 30.50];
+
+// let total = 0;
+// for(let price of prices) {
+    // total += price;
+// }
+// console.log(total)
+
+// const total = prices.reduce((total, price) => {
+    // return total + price
+//})
+
+const total = prices.reduce((total, price) => total + price);
+
+const minPrice = prices.reduce((min, price) => {
+    if(price < min) {
+        return price
+    }
+    return min
+})
+
+// console.log(total)
+// console.log(minPrice)
+
+const compare = movies.reduce((bestMovie, currMovie) => {
+    if(currMovie.score > bestMovie.score) {
+        return currMovie
+    } return bestMovie
+})
+
+//console.log(compare)
+
+const evens = [2,4,6,8];
+
+evens.reduce((sum, num) => sum + num)
+evens.reduce((sum, num) => sum + num, 100)
