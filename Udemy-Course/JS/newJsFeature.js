3d19ffce249f5ff12ee61d5d838642f74ab5919a
anit <anit7795@gmail.com>
2025-06-18 20:40:20 +0530

    Spread

--- Udemy-Course/JS/newJsFeature.js
+++ Udemy-Course/JS/newJsFeature.js
@@ -16,8 +16,15 @@
 
 const nums = [1,324,2,4,234,23,423,423,42,4,4,43,3,2,45,46,456,535,35,2,52];
 
-console.log(Math.max(...nums));
+// console.log(Math.max(...nums));
+// 
+// console.log(...nums);
+// 
+// console.log(...'hello')
 
-console.log(...nums);
+const cats = ['Blue', 'Scout', 'Rocket'];
+const dogs = ['Rusty', 'Wyatt'];
 
-console.log(...'hello')
+const allPets = [...cats, ...dogs]
+
+console.log(allPets)
