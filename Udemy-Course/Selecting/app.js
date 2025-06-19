6bf69784d761a43da1909b40882716c83a1dbc4f
anit <anit7795@gmail.com>
2025-06-20 00:54:20 +0530

    querySelector and querySelectorAll

--- Udemy-Course/Selecting/app.js
+++ Udemy-Course/Selecting/app.js
@@ -13,8 +13,32 @@ const allImages = document.getElementsByTagName('img');
 // 
 const classId = document.getElementsByClassName('square');
 
-for(classes of classId) {
-    classes.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
-}
+// for(classes of classId) {
+    // classes.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
+// }
+// 
+// console.log(classId)
 
-console.log(classId)
+const selector = document.querySelector('p');
+const byTag = document.getElementsByTagName('p');
+const byTagName = document.querySelector('#banner');
+const byClass = document.querySelector('.square');
+const second = document.querySelector('img:nth-of-type(2)');
+
+// console.log(selector);
+// console.log(byTag);
+// console.log(byTagName);
+// console.log(byClass);
+// console.log(second);
+
+const new1 = document.querySelector('a[title="Java"]');
+
+// console.log(new1)
+
+const all = document.querySelectorAll('p');
+const all1 = document.querySelectorAll('p a');
+
+for(let all of all1) {
+    console.log(all.href)
+}
+console.log(all1)
