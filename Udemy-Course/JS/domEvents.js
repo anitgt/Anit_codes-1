3b476db8c2aa3e4d67ab1b5fe5532c95563923ad
anit <anit7795@gmail.com>
2025-06-21 00:25:28 +0530

    this in events

--- Udemy-Course/JS/domEvents.js
+++ Udemy-Course/JS/domEvents.js
@@ -1,42 +1,70 @@
-const button2 = document.querySelector('#v2');
+// const button2 = document.querySelector('#v2');
 
-button2.onclick = function() {
-    console.log('You clicked me;')
-    console.log('I hope it worked!')
+// button2.onclick = function() {
+//     console.log('You clicked me;')
+//     console.log('I hope it worked!')
+// }
+
+// function scream() {
+//     console.log('Ahhhhhh')
+//     console.log('Stop touching me!')
+// }
+
+// button2.onmouseenter = scream;
+
+// document.querySelector('h1') .onclick = function() {
+//      console.log("i am here");
+//     }
+
+// const btn3 = document.querySelector('#v3');
+
+// btn3.addEventListener('click', ()=> {
+//     console.log('Clicked') 
+// });
+
+// function twist() {
+//     console.log('twist')
+// } 
+
+// function shout() {
+//     console.log('Shout')
+// }
+
+// const tasButton = document.querySelector('#tas');
+
+// // tasButton.onclick = twist
+// // tasButton.onclick = shout;
+
+// // tas.onclick = twist
+// // tas.onclick = shout
+
+// tasButton.addEventListener('click', twist, {once: true});
+// tasButton.addEventListener('click', shout);
+
+function makeRandomColor() {
+    const r = Math.floor(Math.random() * 255 + 1)
+    const g = Math.floor(Math.random() * 255 + 1)
+    const b = Math.floor(Math.random() * 255 + 1)
+    const color = `rgb( ${r}, ${g}, ${b})`;
+    return color
 }
 
-function scream() {
-    console.log('Ahhhhhh')
-    console.log('Stop touching me!')
+const buttons = document.querySelectorAll('button');
+
+
+for(let button of buttons) {
+    button.addEventListener('click', colorize)
 }
 
-button2.onmouseenter = scream;
+const h1s = document.querySelectorAll('h1');
 
-document.querySelector('h1') .onclick = function() {
-     console.log("i am here");
-    }
-
-const btn3 = document.querySelector('#v3');
-
-btn3.addEventListener('click', ()=> {
-    console.log('Clicked') 
-});
-
-function twist() {
-    console.log('twist')
-} 
-
-function shout() {
-    console.log('Shout')
+for(let h1 of h1s) {
+    h1.addEventListener('click', colorize)
 }
 
-const tasButton = document.querySelector('#tas');
-
-// tasButton.onclick = twist
-// tasButton.onclick = shout;
-
-// tas.onclick = twist
-// tas.onclick = shout
-
-tasButton.addEventListener('click', twist, {once: true});
-tasButton.addEventListener('click', shout);
+function colorize() {
+    const this1 = this.style.backgroundColor = makeRandomColor();
+    const this2 = this.style.color = makeRandomColor();
+    return this1,  this2
+}
+ 
