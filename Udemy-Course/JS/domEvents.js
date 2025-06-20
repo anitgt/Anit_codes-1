b18deeecc6d7a6bec7859c0095c2ce61a86614c6
anit <anit7795@gmail.com>
2025-06-21 00:45:38 +0530

    Event object

--- Udemy-Course/JS/domEvents.js
+++ Udemy-Course/JS/domEvents.js
@@ -41,30 +41,63 @@
 // tasButton.addEventListener('click', twist, {once: true});
 // tasButton.addEventListener('click', shout);
 
-function makeRandomColor() {
-    const r = Math.floor(Math.random() * 255 + 1)
-    const g = Math.floor(Math.random() * 255 + 1)
-    const b = Math.floor(Math.random() * 255 + 1)
-    const color = `rgb( ${r}, ${g}, ${b})`;
-    return color
-}
+// function makeRandomColor() {
+    // const r = Math.floor(Math.random() * 255 + 1)
+    // const g = Math.floor(Math.random() * 255 + 1)
+    // const b = Math.floor(Math.random() * 255 + 1)
+    // const color = `rgb( ${r}, ${g}, ${b})`;
+    // return color
+// }
+// 
+// const buttons = document.querySelectorAll('button');
+// 
+// 
+// for(let button of buttons) {
+    // button.addEventListener('click', colorize)
+// }
+// 
+// const h1s = document.querySelectorAll('h1');
+// 
+// for(let h1 of h1s) {
+    // h1.addEventListener('click', colorize)
+// }
+// 
+// function colorize() {
+    // const this1 = this.style.backgroundColor = makeRandomColor();
+    // const this2 = this.style.color = makeRandomColor();
+    // return this1,  this2
+// }
+// 
 
-const buttons = document.querySelectorAll('button');
+const button = document.querySelector('button');
 
+button.addEventListener('click', function(evt) {
+    console.log(evt)
+})
 
-for(let button of buttons) {
-    button.addEventListener('click', colorize)
-}
+const input = document.querySelector('input');
 
-const h1s = document.querySelectorAll('h1');
+// input.addEventListener('keydown', (e) => {
+    // console.log(e.key);
+    // console.log(e.code);
+// })
+// input.addEventListener('keyup', () => console.log('Keyup') )
 
-for(let h1 of h1s) {
-    h1.addEventListener('click', colorize)
-}
-
-function colorize() {
-    const this1 = this.style.backgroundColor = makeRandomColor();
-    const this2 = this.style.color = makeRandomColor();
-    return this1,  this2
-}
- 
+window.addEventListener('keydown', (e) => {
+    switch(e.code) {
+        case 'ArrowUp':
+            console.log('UP');
+            break;
+        case 'ArrowDown':
+            console.log('Down');
+            break;
+        case 'ArrowLeft':
+            console.log('Left');
+            break;
+        case 'ArrowRight':
+            console.log('Right');
+            break;
+        default: 
+            console.log('Ignored')
+    }
+})
