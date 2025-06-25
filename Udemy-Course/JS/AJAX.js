559163d0c308169000ed05a7044bc4d22d1930a3
anit <anit7795@gmail.com>
2025-06-26 01:17:19 +0530

    Fetch and async function

--- Udemy-Course/JS/AJAX.js
+++ Udemy-Course/JS/AJAX.js
@@ -24,24 +24,67 @@ const newJSON = JSON.stringify(dog);
 
 //Making XHRs
 
-const req = new XMLHttpRequest();
+// const req = new XMLHttpRequest();
 
-req.onload = function () {
-    console.log('It loaded');
-    // console.log(this.responseText)
+// req.onload = function () {
+//     console.log('It loaded');
+//     // console.log(this.responseText)
 
-    const jsObject = JSON.parse(this.responseText)
-    //console.log(jsObject);
-    console.log(jsObject.name, jsObject.height)
+//     const jsObject = JSON.parse(this.responseText)
+//     //console.log(jsObject);
+//     console.log(jsObject.name, jsObject.height)
+// }
+
+// req.onerror = function() {
+//     console.log('Error');
+//     console.log(this)
+// }
+// req.open("GET", 'https://swapi.info/api/people/1');
+// req.send();
+
+
+//using fetch :-
+// fetch('https://swapi.info/api/people/1')
+//     .then(res => {
+//         console.log('resolved', res);
+//         return res.json()
+//     })
+//     .then((data) => {
+//         console.log(data)
+//         console.log(data.height)
+//         return fetch('https://swapi.info/api/people/2')     
+//     })
+
+//     .then((res2) => {
+//         console.log('Second req done!')
+//         console.log(res2);
+//         return res2.json()
+//     })
+
+//     .then((data) => console.log(data))
+
+//     .catch((e) => {
+//         console.log('Error', e)
+//     })
+
+//using async function :-
+
+const loadStarWarsPeople = async () => {
+    try {
+        const res = await fetch('https://swapi.info/api/people/1');
+        const data = await res.json();
+        console.log(data);
+
+        const res2 = await fetch('https://swapi.info/api/people/2');
+        const data2 = await res2.json();
+        console.log(data2)
+    }
+
+    catch (e) {
+        console.log('Error');
+        console.log(e);
+    }
+    
 }
 
-req.onerror = function() {
-    console.log('Error');
-    console.log(this)
-}
-
-req.open("GET", 'https://swapi.info/api/people/1');
-req.send();
-
-
-
+loadStarWarsPeople()
