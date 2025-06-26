6f9379822e7fcbe9f38d179734a451c14cd7c93f
anit <anit7795@gmail.com>
2025-06-26 21:19:49 +0530

    prototypes

---
+++ Udemy-Course/JS/OOPS.js
@@ -0,0 +1,7 @@
+String.prototype.yell = function() {
+    return `${(this.toUpperCase())} !!!!`;
+}
+
+Array.prototype.pop = function () {
+    return 'Sorry i want that element, i will never pop it off'
+}
