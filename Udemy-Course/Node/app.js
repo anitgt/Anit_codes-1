b65f07840c6be99b05b9d13d723347c64b9a6d94
anit <anit7795@gmail.com>
2025-06-30 21:26:24 +0530

    requiring a directory

--- Udemy-Course/Node/app.js
+++ Udemy-Course/Node/app.js
@@ -1,2 +1,6 @@
 const math = require('./math');
-console.log(math.square(5))
+//console.log(math.square(5));
+
+const cats = require('./shelter');
+
+console.log('Required an entire directory', cats);
