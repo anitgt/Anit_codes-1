bd0e7fe97b61a1eef14ae0bfb0a691b13e22dfe0
anit <anit7795@gmail.com>
2025-07-03 00:45:00 +0530

    req and response object

--- Udemy-Course/Node/FirstApp/index.js
+++ Udemy-Course/Node/FirstApp/index.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const app = express();
+
+// app.use((req, res) => {
+//     console.log('We got a new request');
+//     //res.send('Hello, We got your request! This is the response')
+//     // res.send({
+//     //     name: 'Xyz',
+//     //     age: 19
+//     // })
+//     res.send('<h1>This is my webpage</h1>');
+// });
