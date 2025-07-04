4c87c19aa1cfec5cfb7671149d9fae7150139f4a
anit <anit7795@gmail.com>
2025-07-04 20:41:53 +0530

    Complex subreddit

--- Udemy-Course/Node/Templating_Demo/index.js
+++ Udemy-Course/Node/Templating_Demo/index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const redditData = require('./data.json');
-
-app.use(express.static(path.join(__dirname, 'public')))
+console.log(redditData)
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
@@ -12,28 +11,26 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('/rand', (req, res)=> {
+    const num = Math.floor(Math.random() * 10) +1;
+    res.render('random', {num : num})
+})
+
 app.get('/cats', (req, res) => {
-    const cats = [
-        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
-    ]
+    const cats = ['Silly', 'billy', 'Millie', 'Killy', 'Tommy'];
     res.render('cats', { cats })
 })
 
 app.get('/r/:subreddit', (req, res) => {
-    const { subreddit } = req.params;
+    const {subreddit} = req.params;
     const data = redditData[subreddit];
-    if (data) {
-        res.render('subreddit', { ...data });
+    if(data) {
+        res.render('subreddit', { ...data })
     } else {
-        res.render('notfound', { subreddit })
+        res.render('notfound', {subreddit})
     }
 })
 
-app.get('/rand', (req, res) => {
-    const num = Math.floor(Math.random() * 10) + 1;
-    res.render('random', { num })
-})
-
 app.listen(3000, () => {
-    console.log("LISTENING ON PORT 3000")
+    console.log('Listening on port 3000')
 })
