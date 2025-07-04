1d11ddb3428a5b3aa28a7964294782ab8a7f4166
anit <anit7795@gmail.com>
2025-07-05 00:46:24 +0530

    Defining get and post req

--- Udemy-Course/Node/RestfulRoutes/index.js
+++ Udemy-Course/Node/RestfulRoutes/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.get('/tacos', (req,res) => {
+    res.send('Get /Tacos response')
+})
+
+app.post('/tacos', (req, res) => {
+    res.send('Post tacos response')
+})
+
 app.listen(3000, () => {
     console.log('On port 3000')
-})
+}) 
