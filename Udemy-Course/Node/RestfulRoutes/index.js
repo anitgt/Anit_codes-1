3f299cdf4204bb8665e52761991bf53740582057
anit <anit7795@gmail.com>
2025-07-05 14:52:29 +0530

    parse req body

--- Udemy-Course/Node/RestfulRoutes/index.js
+++ Udemy-Course/Node/RestfulRoutes/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.get('/tacos', (req,res) => {
     res.send('Get /Tacos response')
 })
 
 app.post('/tacos', (req, res) => {
-    res.send('Post tacos response')
+    const { meat, qty} = req.body;
+    res.send(`Ok! Here are your ${qty} of ${meat} `)
 })
 
 app.listen(3000, () => {
-    console.log('On port 3000')
+    console.log('On port 3000');
 }) 
