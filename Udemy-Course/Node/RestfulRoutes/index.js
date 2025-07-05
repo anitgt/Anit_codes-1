dc74d359a9bf9dcbdf516acccbfc5f75ad612a32
anit <anit7795@gmail.com>
2025-07-05 17:22:19 +0530

    Express redirects

--- Udemy-Course/Node/RestfulRoutes/index.js
+++ Udemy-Course/Node/RestfulRoutes/index.js
@@ -34,7 +34,7 @@ app.get('/comments', (req, res) => {
 app.post('/comments', (req, res) => {
     const { username, comment}  = req.body;
     comments.push({username , comment})
-        res.send('It worked')
+        res.redirect('/comments')
 })
 
 app.get('/comments/new', (req, res) => {
