7229d7f260594ff9a5d948e50a5025012d2578bd
anit <anit7795@gmail.com>
2025-07-05 15:52:00 +0530

    Restful Comments

--- Udemy-Course/Node/RestfulRoutes/index.js
+++ Udemy-Course/Node/RestfulRoutes/index.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
 app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.json());
+
+app.set('views',path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'Nice one'
+    },
+    {
+        username: 'Bomm',
+        comment: 'Hello'
+    },
+    {
+        username: 'lil baby',
+        comment: 'Nice drip ma boy'
+    },
+    {
+        username: 'Travis scott',
+        comment: 'La flame'
+    },
+];
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', {comments})
+})
 
 app.get('/tacos', (req,res) => {
     res.send('Get /Tacos response')
 })
+
 
 app.post('/tacos', (req, res) => {
     const { meat, qty} = req.body;
