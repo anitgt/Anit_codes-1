3639a9190d08aadea6bcea872803afbd441a6619
anit <anit7795@gmail.com>
2025-07-09 22:49:19 +0530

    Mongoose virtuals

---
+++ Udemy-Course/Node/Mongoose/person.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/personApp')
+    .then(() => {
+        console.log('Connection Open!!')
+    })
+    .catch(err => {
+        console.log("Error");
+        console.log(err)
+    });
+
+const personSchema = new mongoose.Schema({
+    first: String,
+    last: String
+});
+
+personSchema.virtual('fullName').get(function() {
+    return `${this.first} ${this.last}`
+})
+
+const Person = mongoose.model('Person', personSchema);
+
+// const testPerson = new Person({
+//     first: 'Tony',
+//     last: 'Stark'
+// })
+
+// testPerson.save().then(() => {
+//     console.log('Sabed to database')
+// })
