d82c2035d63bf4ec23a8c57eb7f0fdb10903e09d
anit <anit7795@gmail.com>
2025-07-09 16:28:27 +0530

    Additional schema contraints

--- Udemy-Course/Node/Mongoose/product.js
+++ Udemy-Course/Node/Mongoose/product.js
@@ -11,20 +11,37 @@ mongoose.connect('mongodb://127.0.0.1:27017/shopApp')
 const productSchema = new mongoose.Schema({
         name: {
             type: String,
-            required: true
+            required: true,
+            maxlength: 20
         },
         price: {
             type: Number,
-            require: true
+            require: true,
+            min: 0
+        },
+        onSale: {
+            type: Boolean,
+            default: false
+        },
+        categories: [String],
+        qty: {
+            online: {
+                type: Number,
+                default: 0
+            },
+            inStore: {
+                type: Number,
+                default: 0
+            }
         }
     })
 
 const Product = mongoose.model('Product', productSchema);
 
 const bike = new Product({
-    name: 'Mountain Bike',
-    price: 999,
-    color: 'Blue'
+    name: 'Bike helmet',
+    price: 29.50,
+    categories: ['cycling', 'Walking', 'Flying'],
 })
 
 bike.save()
