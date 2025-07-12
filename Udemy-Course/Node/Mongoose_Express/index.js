ee2a986df27dc63f1bafb7b673b8b9cfc15bbcd6
anit <anit7795@gmail.com>
2025-07-13 00:35:35 +0530

    updating products

--- Udemy-Course/Node/Mongoose_Express/index.js
+++ Udemy-Course/Node/Mongoose_Express/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-
+const methodOverride = require('method-override')
 const Product = require('./models/product');
 
 main().then(() => {
@@ -21,7 +21,9 @@ async function main() {
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 app.get('/products', async(req, res) => {
     const products = await Product.find({});
@@ -51,7 +53,10 @@ app.get('/products/:id/edit', async (req,res) => {
 })
 
 app.put('/products/:id', async (req,res) => {
-    
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
+    res.redirect(`/products/${product._id}`)
+    console.log(req.body)
 })
 
 app.listen(3000, () => {
