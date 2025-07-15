4d0f10e8f1bd37009c04d0030a21cacd6ddbd9f1
anit <anit7795@gmail.com>
2025-07-15 19:54:27 +0530

    Morgan

--- Udemy-Course/Middleware_intro/index.js
+++ Udemy-Course/Middleware_intro/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 
+// morgan('tiny')
 
+app.use(morgan('tiny'))
+
+app.get('/', (req, res) => {
+    res.send('On');
+});
+app.get('/dogs', (req, res) => {
+    res.send('Woof Woof');
+});
 
 app.listen(3000,()=> {
     console.log('Listening at port 3000!')
