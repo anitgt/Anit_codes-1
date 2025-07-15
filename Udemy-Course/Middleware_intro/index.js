eb45871818b6629bd3b6344ee1692e644c0233b1
anit <anit7795@gmail.com>
2025-07-15 20:22:26 +0530

    middleware practice

--- Udemy-Course/Middleware_intro/index.js
+++ Udemy-Course/Middleware_intro/index.js
@@ -4,25 +4,34 @@ const morgan = require('morgan')
 
 // morgan('tiny')
 
-app.use(morgan('tiny'))
-app.use((req, res,next) => {
-    console.log('This is my first middleware');
-    return next();
-    console.log('this is my first middleware after calling next()!')
-})
-app.use((req, res,next) => {
-    console.log('This is my second middleware');
-    return next()
-})
-app.use((req, res,next) => {
-    console.log('This is my third middleware');
-    return next()
+app.use(morgan('tiny'));
+
+app.use((req,res,next) => {
+    req.requestTime = Date.now()
+    console.log(req.method, req.path);
+    next()
 })
 
+// app.use((req, res,next) => {
+//     console.log('This is my first middleware');
+//     return next();
+//     console.log('this is my first middleware after calling next()!')
+// })
+// app.use((req, res,next) => {
+//     console.log('This is my second middleware');
+//     return next()
+// })
+// app.use((req, res,next) => {
+//     console.log('This is my third middleware');
+//     return next()
+//})
+
 app.get('/', (req, res) => {
+    console.log(`Request Date = ${req.reqTime}`)
     res.send('On');
 });
 app.get('/dogs', (req, res) => {
+    console.log(`Request Date = ${req.reqTime}`)
     res.send('Woof Woof');
 });
 
