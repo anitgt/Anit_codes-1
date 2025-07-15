fb26929aae683540ee0613f87dce4e397b665b79
anit <anit7795@gmail.com>
2025-07-15 20:28:25 +0530

    404 route

--- Udemy-Course/Middleware_intro/index.js
+++ Udemy-Course/Middleware_intro/index.js
@@ -12,6 +12,11 @@ app.use((req,res,next) => {
     next()
 })
 
+app.use('/dogs', (req,res,next) => {
+    console.log('i love dogs!');
+    next()
+})
+
 // app.use((req, res,next) => {
 //     console.log('This is my first middleware');
 //     return next();
@@ -34,6 +39,10 @@ app.get('/dogs', (req, res) => {
     console.log(`Request Date = ${req.reqTime}`)
     res.send('Woof Woof');
 });
+
+app.use((req,res) => {
+    res.status(404).send('Not found!')
+})
 
 app.listen(3000,()=> {
     console.log('Listening at port 3000!')
