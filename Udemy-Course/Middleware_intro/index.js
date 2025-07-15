46af081b3a7c3f8335f64130895ebdd5a066d4a4
anit <anit7795@gmail.com>
2025-07-15 20:06:01 +0530

    defining middleware

--- Udemy-Course/Middleware_intro/index.js
+++ Udemy-Course/Middleware_intro/index.js
@@ -5,6 +5,19 @@ const morgan = require('morgan')
 // morgan('tiny')
 
 app.use(morgan('tiny'))
+app.use((req, res,next) => {
+    console.log('This is my first middleware');
+    return next();
+    console.log('this is my first middleware after calling next()!')
+})
+app.use((req, res,next) => {
+    console.log('This is my second middleware');
+    return next()
+})
+app.use((req, res,next) => {
+    console.log('This is my third middleware');
+    return next()
+})
 
 app.get('/', (req, res) => {
     res.send('On');
