173a1a532061e108e75513c86f13ac080fadc20c
anit <anit7795@gmail.com>
2025-07-20 22:47:00 +0530

    One to few

--- Udemy-Course/Mongo_Relationships/Models/user.js
+++ Udemy-Course/Mongo_Relationships/Models/user.js
@@ -14,3 +14,48 @@ mongoose.connect(MONGO_URI, {
     console.error("MongoDB connection error:", err);
 });
 
+const userSchema = new mongoose.Schema({
+    first: String,
+    last: String,
+    addresses : [
+        {
+            _id: {_id: false},
+            street: String,
+            city: String,
+            state: String,
+            country: String
+}]
+})
+
+
+const User = mongoose.model('user', userSchema);
+
+const makeUser = async () => {
+    const u = new User({
+        first: 'Harry',
+        last: 'Potter'
+    })
+    u.addresses.push({
+        street: '123',
+        city: 'Xyz',
+        state: 'mh',
+        country: 'india'
+    })
+    const res = await u.save()
+    console.log(res)
+
+}
+
+const addAddress = async (id) => {
+    const user = await User.findById(id);
+    user.addresses.push({
+        street: '12qeweqwe3',
+        city: 'Xeyeewewz',
+        state: 'meewqh',
+        country: 'inqwediea'
+    })
+    const res = await user.save();
+    console.log(res)
+}
+
+addAddress('687d2384b578a82d81eb48f3')
