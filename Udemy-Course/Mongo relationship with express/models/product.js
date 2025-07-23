8329153c21300a338a365dd7b7f30e485550ef43
anit <anit7795@gmail.com>
2025-07-23 20:45:41 +0530

    Defining Farm and Products Model

--- Udemy-Course/Mongo relationship with express/models/product.js
+++ Udemy-Course/Mongo relationship with express/models/product.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const productSchema = new mongoose.Schema({
     name: {
-        type: 'String',
+        type: String,
         required: true
     },
     price: {
@@ -14,9 +15,13 @@ const productSchema = new mongoose.Schema({
         type: String,
         lowercase: true,
         enum: ['fruit', 'vegetable', 'dairy']
+    },
+    farm: {
+        type: Schema.Types.ObjectId,
+        ref: 'Farm'
     }
 })
 
-const Product = mongoose.model('product', productSchema);
+const Product = mongoose.model('Product', productSchema);
 
 module.exports = Product;
