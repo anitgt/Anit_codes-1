982c6558ecaf9d736fd3b2cb2b521035f80d350a
anit <anit7795@gmail.com>
2025-07-26 01:42:01 +0530

    signing cookies

--- Udemy-Course/cookies/index.js
+++ Udemy-Course/cookies/index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser')
+
+app.use(cookieParser('secretcode1'))
+
 app.get('/greet', (req,res) => {
-    res.send('Hey there!');
+    const { name = 'Person' } = req.cookies
+    // res.send('Hey there!');
+    res.send(`Helllo ${name}`)
 })
 
 app.get('/setname', (req,res) => {
     res.cookie('name', 'Alex Jack');
     res.cookie('age', '20')
     res.send('Sent you a cookie')
+})
+
+app.get('/getsignedcookie', (req,res) => {
+    res.cookie('fruit', 'Grape', { signed: true } );
+    res.send('Signing successful')
+})
+
+app.get('/verifyfruit', (req,res) => {
+    // console.log(req.cookies);
+    res.send(req.signedCookies)
 })
 
 app.listen(3000, () => {
