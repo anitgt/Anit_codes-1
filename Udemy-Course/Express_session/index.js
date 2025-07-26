bce6ecafe3bfe0c378469f2aa8ee52c1d7522fd5
anit <anit7795@gmail.com>
2025-07-26 16:09:26 +0530

    practice session

--- Udemy-Course/Express_session/index.js
+++ Udemy-Course/Express_session/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const session = require('express-session')
 
-app.use(session({secret: 'secret1'}));
+const sessionOptions = {secret: 'secret1', resave: false, saveUninitialized: false}
+app.use(session(sessionOptions));
 
 app.get('/viewcount', (req,res) => {
     if(req.session.count) {
@@ -11,6 +12,17 @@ app.get('/viewcount', (req,res) => {
         req.session.count = 1;
     }
     res.send(`You have viewed this page: ${req.session.count} times!`)
+})
+
+app.get('/register', (req,res) => {
+    const { username = 'unknown' } = req.query;
+    req.session.username = username;
+    res.redirect('/greet')
+});
+
+app.get('/greet', (req,res) => {
+    const { username } = req.session;
+    res.send(`Welcome back ${username}`)
 })
 
 app.listen(3000, () => {
