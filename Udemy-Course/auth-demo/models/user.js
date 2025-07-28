031900c055926fd36d1feb0c555442e4dff35676
anit <anit7795@gmail.com>
2025-07-28 22:55:38 +0530

    Auth form

---
+++ Udemy-Course/auth-demo/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Username Cannot be blank!'],
+    },
+    password: {
+        type: Number,
+        required: [true, 'Password cannot be blank!']
+    }
+});
+
+module.exports = mongoose.model('User', userSchema)
