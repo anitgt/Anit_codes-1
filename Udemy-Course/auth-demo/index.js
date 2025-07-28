7c8619bbbe05aa854a1dd2f4a9d175cc393ddadc
anit <anit7795@gmail.com>
2025-07-28 23:49:57 +0530

    login middleware

--- Udemy-Course/auth-demo/index.js
+++ Udemy-Course/auth-demo/index.js
@@ -19,6 +19,12 @@ app.set('views', 'views');
 app.use(express.urlencoded( {extended: true }));
 app.use(session({secret: 'secret1'}))
 
+const requireLogin = (req,res,next) => {
+    if(!req.session.user_id) {
+        return res.redirect('/login');
+    }
+    next();
+}
 
 app.get('/', (req,res) => {
     res.send('This is the homepage!')
@@ -63,11 +69,12 @@ app.post('/logout', (req,res) => {
     res.redirect('/login')
 })
 
-app.get('/secret', (req,res) => {
-    if(!req.session.user_id){
-       return  res.redirect('/login');
-    }
+app.get('/secret', requireLogin, (req,res) => {
     res.render('secret')
+});
+
+app.get('/newsecret', requireLogin, (req,res) => {
+    res.send('Yo')
 })
 
 app.listen(3000, () => {
