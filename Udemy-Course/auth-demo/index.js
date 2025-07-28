3f1c0a6cf0f3e45535a1f6fbecfc29ec1a304e34
anit <anit7795@gmail.com>
2025-07-29 00:13:03 +0530

    Refactoring the code

--- Udemy-Course/auth-demo/index.js
+++ Udemy-Course/auth-demo/index.js
@@ -41,11 +41,8 @@ app.get('/login', (req,res) => {
 
 app.post('/register', async (req,res) => {
     const { password, username } = req.body;
-    const hash = await bcrypt.hash(password, 12);
-    const user = new User({
-        username: username,
-        password: hash
-    });
+    
+    const user = new User({username: username,password});
     await user.save();
     req.session.user_id = user._id;
     res.redirect('/')
@@ -53,10 +50,9 @@ app.post('/register', async (req,res) => {
 
 app.post('/login', async (req,res) => {
     const { username, password } = req.body;
-    const user = await User.findOne( {username });
-    const validPassword = await bcrypt.compare(password, user.password);
-    if(validPassword) {
-        req.session.user_id = user._id;
+    const foundUser = await User.findAndValidate(username, password);
+    if(foundUser) {
+        req.session.user_id = foundUser._id;
         res.redirect('/secret')
     } else {
         res.redirect('/login')
