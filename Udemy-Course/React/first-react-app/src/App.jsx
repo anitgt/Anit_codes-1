78fe36ad11976e9205dd66f57342797b1af0fb73
anit <anit7795@gmail.com>
2025-09-08 20:54:12 +0530

    React Conditionals

--- Udemy-Course/React/first-react-app/src/App.jsx
+++ Udemy-Course/React/first-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Chicken from './chicken'
 import Greeter from './Greeter'
 import Die from './Die'
 import ListPicker from "./ListPicker";
+import DoubleDice from './DoubleDice';
 
 function App() {
   return (
@@ -18,8 +19,11 @@ function App() {
       <Die num={30}/>
       <Die num={40}/>
       <Die/> */}
-      <ListPicker values={[1,2,3]} />
-      <ListPicker values={["a", "b", "c"]} />
+      {/* <ListPicker values={[1,2,3]} />
+      <ListPicker values={["a", "b", "c"]} /> */}
+      <DoubleDice />
+      <DoubleDice />
+      <DoubleDice />
     </>
   )
 }
---
+++ Udemy-Course/React/first-react-app/src/DoubleDice.jsx
@@ -0,0 +1,49 @@
+export default function DoubleDice() {
+    const num1 = Math.floor(Math.random() *3) + 1;
+    const num2 = Math.floor(Math.random() *3) + 1;
+    return (
+        <div>
+            <h2>Double Dice:</h2>
+            {num1 === num2 && <h3>You Win!</h3>}
+            <p>Number 1: {num1} & Number 2: {num2}</p>
+        </div>
+    )
+}
+
+
+// export default function DoubleDice() {
+//     const num1 = Math.floor(Math.random() *3) + 1;
+//     const num2 = Math.floor(Math.random() *3) + 1;
+//     return (
+//         <div>
+//             <h2>Double Dice:</h2>
+//             {num1 === num2 ? <h3>You Win!</h3> : null}
+//             <p>Number 1: {num1} & Number 2: {num2}</p>
+//         </div>
+//     )
+// }
+
+
+// export default function DoubleDice() {
+//     const num1 = Math.floor(Math.random() *3) + 1;
+//     const num2 = Math.floor(Math.random() *3) + 1;
+//     return (
+//         <div>
+//             <h2>{ num1 === num2 ? "You Win!" : "You Loose!"}</h2>
+//             <p>Number 1: {num1} & Number 2: {num2}</p>
+//         </div>
+//     )
+// }
+
+
+// export default function DoubleDice() {
+//     const num1 = Math.floor(Math.random() *3) + 1;
+//     const num2 = Math.floor(Math.random() *3) + 1;
+//     const result = num1 === num2 ? "You Win!" : "You Loose!"
+//     return (
+//         <div>
+//             <h2>{result}</h2>
+//             <p>Number 1: {num1} & Number 2: {num2}</p>
+//         </div>
+//     )
+// }
