3919de337ee1518d4eb4f79087f35f555ea7c793
anit <anit7795@gmail.com>
2025-09-08 11:15:28 +0530

    Passing arrays and objects

--- Udemy-Course/React/first-react-app/src/App.jsx
+++ Udemy-Course/React/first-react-app/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Chicken from './chicken'
 import Greeter from './Greeter'
- import Die from './Die'
+import Die from './Die'
+import ListPicker from "./ListPicker";
+
 function App() {
   return (
     <>
-      <Chicken/>
+      {/* <Chicken/>
       <Chicken/>
       <Chicken/>
       <Greeter person='Bill' from='Anit'/>
@@ -15,7 +17,9 @@ function App() {
       <Die num={20}/>
       <Die num={30}/>
       <Die num={40}/>
-      <Die/>
+      <Die/> */}
+      <ListPicker values={[1,2,3]} />
+      <ListPicker values={["a", "b", "c"]} />
     </>
   )
 }
