ff84497db20ddc9aec869e52059df806e8071b77
anit <anit7795@gmail.com>
2025-09-14 16:30:34 +0530

    when renders

--- Udemy-Course/React/state-demo/src/Counter.jsx
+++ Udemy-Course/React/state-demo/src/Counter.jsx
@@ -2,9 +2,13 @@ import { use, useState } from 'react'
 
 
 function Counter() {
+    console.log('Counter rendered')
     const [count, setCount] = useState(0);
     function handleIncrementBy1() {
         setCount(count + 1);
+    }
+    const setToTen = () => {
+        setCount(10)
     }
     function handleIncrementBy3() {
         setCount(currentCount => currentCount + 1);
@@ -16,6 +20,7 @@ function Counter() {
         <p>{count}</p>
         <button onClick={handleIncrementBy1}>Increment +1</button>
         <button onClick={handleIncrementBy3}>Increment +3</button>
+        <button onClick={setToTen}>Set to ten!</button>
         </div>
     )
 }
