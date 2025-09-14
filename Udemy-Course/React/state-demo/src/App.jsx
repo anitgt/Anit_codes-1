b0568b6b6ac80b11af286fec574f0eeb556590d4
anit <anit7795@gmail.com>
2025-09-14 16:47:08 +0530

    working with object in state

--- Udemy-Course/React/state-demo/src/App.jsx
+++ Udemy-Course/React/state-demo/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Counter from './Counter'
 import Practice from './practice'
+import ScoreKeeper from './ScoreKeeper'
 
 function App() {
   return (
     <>
      {/* <h1>State Demo</h1> */}
-      <Counter />
+      {/* <Counter /> */}
       {/* <Practice /> */}
+      <ScoreKeeper />
     </>
   )
 }
