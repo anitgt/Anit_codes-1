ad673f9be721e000005d1e0e678c750096ac773d
anit <anit7795@gmail.com>
2025-12-11 01:56:09 +0530

    Adding a loader

--- Udemy-Course/React/Effects/src/App.jsx
+++ Udemy-Course/React/Effects/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import Counter from './counter'
 import QuoteFetcher from './QuoteFetcher'
+import QuoteFetcherLoader from './QuoteFetcherLoader'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <QuoteFetcher/>
+     <QuoteFetcherLoader/>
     </>
   )
 }
