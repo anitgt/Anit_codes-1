b82ec3f375bd6f124c20926ace24e0b6088e8bf1
anit <anit7795@gmail.com>
2025-12-11 20:15:53 +0530

    The rating component

--- Udemy-Course/React/Material-Ui/src/App.jsx
+++ Udemy-Course/React/Material-Ui/src/App.jsx
@@ -4,19 +4,23 @@ import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import AlarmIcon  from "@mui/icons-material/Alarm"
 
+import RatingDemo from './RatingDemo'
+
 function App() {
 
   return (
     <>
       <div>
-        <Button variant="contained"onClick={()=> alert("working")} >Contained</Button>
+        {/* <Button variant="contained"onClick={()=> alert("working")} >Contained</Button>
         <Button variant="text">Contained</Button>
         <Button variant="outlined">Contained</Button>
         <Button color='success' size='small' variant="outlined">Contained</Button>
       
         <IconButton color="secondary" aria-label="add an alarm">
           <AlarmIcon/>
-        </IconButton>
+        </IconButton> */}
+
+        <RatingDemo/>
       </div>
     </>
   )
