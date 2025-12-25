21f648a59ef4f9824942974501ea96e4ff1a058f
anit <anit7795@gmail.com>
2025-12-25 11:05:17 +0530

    typedef or using keyword

---
+++ C++/practice2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+//typedef std::vector<std::pair<std::string, int>> pairlist_t;
+//typedef std::string text_t;
+//typedef int number_t;
+
+using text_t = std::string;
+using number_t = int;
+
+
+int main(){
+    
+    //pairlist_t;
+
+    text_t firstname = "John";
+
+    number_t  in2 = 8;
+
+    std::cout << firstname << '\n';
+    std::cout << in2 << '\n';
+
+    return 0;
+}
