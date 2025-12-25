cbaf0ccd080e6334cb23f40c46b6d8091a703743
anit <anit7795@gmail.com>
2025-12-25 09:59:35 +0530

    Studying  c++

---
+++ C++/helloworld.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main () {
+
+    int x = 8 ;// store only absolute value
+
+
+
+    double days = 7.5; //store including integer
+    double price  = 9.99;
+    double temperature = 18.8;
+
+    //single character 
+
+    char grade = 'A';
+    char initial = 'B';
+    char currency = '$';
+
+    //boolean character
+
+    bool student = false;
+    bool power = true;
+    bool forSale = false;
+
+    //string characters (objects which represents sequence of text)
+
+    std::string name = "Anit";
+    std::string day = "friday";
+    std::string address = "123 Street";
+
+    std::cout << "Hello " << name << " How are you today?";
+
+
+    return 0;
+}
