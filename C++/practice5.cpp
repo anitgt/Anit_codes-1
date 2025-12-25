3de5ad4be4a0059ee99668cfab0c1a7efd59deb3
anit <anit7795@gmail.com>
2025-12-25 13:33:10 +0530

    Accept user inputs

---
+++ C++/practice5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+
+    std::string name;
+    int age;
+
+    std::cout << "What is your age?";
+    std::cin >> age;
+
+    std::cout << "Whats your full namne?: " ;
+    std::getline(std::cin >> std::ws, name);  
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old!";
+
+    return 0;
+}
