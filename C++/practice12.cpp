97c084c900fc376a67673dff5130ab91c2a14b5c
anit <anit7795@gmail.com>
2026-04-02 01:55:26 +0530

    logical operators in cpp

---
+++ C++/practice12.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+
+    int temp;
+    bool sunny;
+
+    std:: cout << "Enter the temperature: ";
+    std::cin >> temp;
+
+    if(temp <= 0 || temp >= 30) {
+        std::cout << "The temperature is bad!\n";
+    } else {
+        std::cout << "The temperature is good!\n";
+    }
+
+    if(sunny) {
+        std::cout << "It is sunny outside";
+    }else {
+        std::cout << "It is cloudy outside";
+    }
+
+    return 0;
+}
