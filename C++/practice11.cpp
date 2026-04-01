ee6c49e8b6941d9d8e0a86a7b0cb064a8ede428c
anit <anit7795@gmail.com>
2026-04-02 01:31:25 +0530

    ternary operator

---
+++ C++/practice11.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+     /*
+    int grade = 71;
+
+    grade >= 75 ? std::cout << "You pass!" : std::cout << "You fail!";
+
+    */
+
+    // int num = 31;
+
+    // num % 2 ? std::cout << "Num is even!" : std::cout << "Num is odd!";
+    
+     bool hungry = true;
+
+    // hungry ? std::cout << "Hungry" : std::cout << "Full";
+
+    std::cout << (hungry ? "You are hungry" : "You are full!");
+
+
+    return 0;
+}
