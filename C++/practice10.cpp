8f1848420beba77495fedf8b0f1c0fd1d4e60f48
anit <anit7795@gmail.com>
2026-04-02 01:16:28 +0530

    console calculator in cpp

---
+++ C++/practice10.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() {
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+    std::cout << "********* CALCULATOR ********* " << '\n';
+
+    std::cout << "Enter either (+ , - , * , /): " ;
+    std::cin >> op;
+
+    std::cout << "Enter #1: ";
+    std::cin >> num1;
+
+    std::cout << "Enter #2: ";
+    std::cin >> num2;
+
+    if(op == '+') {
+        result = num1 + num2;
+    } else if (op == '-') {
+        result = num1 - num2;
+    } else if (op == '*') {
+        result = num1 * num2;
+    } else if (op == '/') {
+        result = num1 /num2;
+    } else {
+        std::cout << "Wrong input!";
+    };
+
+    std::cout << "result: " << result << '\n';
+
+    std::cout << "*****************************************";
+
+
+    return 0;
+}
