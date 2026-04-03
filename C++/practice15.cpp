ea030e32706651a95328df7536ef9f87b8967a51
anit <anit7795@gmail.com>
2026-04-03 15:07:41 +0530

    While loop in cpp

---
+++ C++/practice15.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+
+    std::string name;
+
+    // while(name.empty()) {
+    //     std::cout << "Enter your name: ";
+    //     std::getline(std::cin, name);
+    // }
+
+    while(1==1) {
+        //std::cout << "Help im stuck in an infinite loop!";
+    };
+
+    std::cout << "Hello " << name;
+
+
+    return 0;
+}
