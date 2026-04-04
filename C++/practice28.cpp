2cfc515fdb5bcf70d083318376d100c904672390
anit <anit7795@gmail.com>
2026-04-04 20:32:26 +0530

    Arrays in cpp

---
+++ C++/practice28.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+/*
+
+int main() {
+    std::string cars[3];
+
+    cars[0] = "Hyundai";
+    cars[1] = "Lambo";
+    cars[2] = "Honda";
+
+    std::cout << cars[0] <<'\n';
+    std::cout << cars[1] << '\n';
+    std::cout << cars[2];
+
+    return 0;
+}
+    */
+
+int main() {
+    double prices[] = {5.00, 7.50, 9.11, 9.14};
+
+    std::cout << prices[0] << "\n";
+    std::cout << prices[1] << "\n";
+    std::cout << prices[2] << "\n";
+    std::cout << prices[3] << "\n";
+    return 0;
+}
