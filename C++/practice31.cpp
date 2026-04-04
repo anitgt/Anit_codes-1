8b166bff910a7a31bc2528ec1e2fb95deb7fc83a
anit <anit7795@gmail.com>
2026-04-04 21:03:01 +0530

    For each loop in cpp

---
+++ C++/practice31.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    // std::string students[] = {"Spongebob", "Patrick", "Squidward", "Rock"};
+
+    // for(std::string student : students) {
+    //     std::cout << student << '\n';
+    // }
+
+    char grades[] = {'A', 'B', 'C', 'D'};
+
+    for(char grade : grades) {
+        std::cout << grade << '\n';
+    }
+
+
+    return 0;
+}
