d980e5e54bde7dccf33491f84026c27d83c4e04c
anit <anit7795@gmail.com>
2026-04-06 20:52:03 +0530

    memory addresses

---
+++ C++/practice39.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string name = "Anit";
+    int age = 21;
+    bool student = true;
+    
+    cout << &name << '\n';
+    cout << &age << '\n';
+    cout << &student << '\n';
+
+    return 0;
+}
