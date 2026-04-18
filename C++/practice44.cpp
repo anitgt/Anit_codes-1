9f6493b4076a322db283f74eebc89d554b90ea84
anit <anit7795@gmail.com>
2026-04-18 23:22:21 +0530

    nullptr behaviour in cpp

---
+++ C++/practice44.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int *pointer = nullptr;
+    int x = 123;
+
+    pointer = &x;
+
+    if(pointer == nullptr) {
+        cout << "address was not assigned! \n";
+    } else {
+        cout << "address was assigned! \n";
+        cout << *pointer;
+    }
+    return 0;
+}
