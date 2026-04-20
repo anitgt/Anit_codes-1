90095291863bbc48abf3375b0e702718c90167fe
anit <anit7795@gmail.com>
2026-04-20 19:10:42 +0530

    Abstraction in cpp with getters and setters

---
+++ C++/practice55.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+class Stove {
+    private: 
+        int temperature = 0;
+
+    public:
+        Stove(int temperature) {
+            setTemperature(temperature);
+        }
+
+        int getTemperature() {
+            return temperature;
+        }
+
+        void setTemperature(int temperature) {
+            if(temperature < 0) {
+                this-> temperature = 0;
+            } else if(temperature >=10 ) {
+                this-> temperature =10;
+            } else {
+                this-> temperature = temperature;
+            }
+            
+        }
+
+};
+
+int main() {
+
+     Stove stove(50);
+
+    // stove.temperatute = 1000;
+
+    //stove.setTemperature(5);
+    
+    cout << "The temperature of the stove is: " << stove.getTemperature();
+
+    return 0;
+}
