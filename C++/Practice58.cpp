0f1c31233e03956c56c351469841fcf31537643d
anit <anit7795@gmail.com>
2026-05-15 15:20:17 +0530

    pattern 4

--- C++/Practice58.cpp
+++ C++/Practice58.cpp
@@ -29,6 +29,15 @@ void printPattern3(int n) {
     };
 }
 
+void printPattern4(int n) {
+    for(int i =1; i <n; i++) {
+        for(int j=1; j <=i; j++) {
+            cout << i << " ";
+        };
+        cout << endl;
+    }
+}
+
 int main() {
     int t;
     cout << "Enter test case no: ";
@@ -37,6 +46,6 @@ int main() {
     int n;
     cout << "Enter a input number for pattern: ";
     cin >> n;
-    printPattern3(n);
+    printPattern4(n);
     }
 }
