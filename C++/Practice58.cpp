c4b1aa23bbc642fcf01102bd95d63cab230f204f
anit <anit7795@gmail.com>
2026-05-15 15:42:07 +0530

    pattern 5

--- C++/Practice58.cpp
+++ C++/Practice58.cpp
@@ -30,9 +30,17 @@ void printPattern3(int n) {
 }
 
 void printPattern4(int n) {
-    for(int i =1; i <n; i++) {
+    for(int i =1; i <=n; i++) {
         for(int j=1; j <=i; j++) {
             cout << i << " ";
+        };
+        cout << endl;
+    }
+}
+void printPattern5(int n) {
+    for(int i =1; i <=n; i++) {
+        for(int j = 1; j <= n - i +1; j++) {
+            cout << "*";
         };
         cout << endl;
     }
@@ -46,6 +54,6 @@ int main() {
     int n;
     cout << "Enter a input number for pattern: ";
     cin >> n;
-    printPattern4(n);
+    printPattern5(n);
     }
 }
