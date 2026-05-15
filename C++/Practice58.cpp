297c5c418a1f0920be86b88d62ad197b1d20727c
anit <anit7795@gmail.com>
2026-05-15 15:01:25 +0530

    Pattern printing using loops, pattern 1 and 2

---
+++ C++/Practice58.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+void printPattern(int n) {
+      for(int i = 0; i <= n; i++) {
+        for(int j = 0; j <= n; j++) {
+            cout << "* ";
+        };
+        cout << endl;
+    }
+}
+
+void printPattern2(int n) {
+    for(int i =0 ; i < n; i++) {
+        for(int j =0; j <= i; j++) {
+            cout << "*";
+        };
+
+        cout << endl;;
+    }
+}
+
+int main() {
+    int t;
+    cout << "Enter test case no: ";
+    cin >> t;
+    for(int i=0; i<t ;i++) {
+    int n;
+    cout << "Enter a input number for pattern: ";
+    cin >> n;
+    //printPattern(n);
+    printPattern2(n);
+    }
+}
