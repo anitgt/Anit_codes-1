8c0dd30da0f8fa4ba7b0ecfe60dd8ab1563f6986
anit <anit7795@gmail.com>
2026-05-15 15:15:50 +0530

    pattern 3

--- C++/Practice58.cpp
+++ C++/Practice58.cpp
@@ -9,16 +9,24 @@ void printPattern(int n) {
         };
         cout << endl;
     }
-}
+};
 
 void printPattern2(int n) {
     for(int i =0 ; i < n; i++) {
         for(int j =0; j <= i; j++) {
             cout << "*";
         };
-
         cout << endl;;
     }
+};
+
+void printPattern3(int n) {
+    for(int i = 1; i <= n; i++) {
+        for(int j=1; j<=i; j++) {
+            cout << j << " ";
+        };
+        cout << endl;
+    };
 }
 
 int main() {
@@ -29,7 +37,6 @@ int main() {
     int n;
     cout << "Enter a input number for pattern: ";
     cin >> n;
-    //printPattern(n);
-    printPattern2(n);
+    printPattern3(n);
     }
 }
