1c1fe470add9ef4f551c22dff2a56a72a43d4acf
anit <anit7795@gmail.com>
2026-05-17 16:52:31 +0530

    pattern 20

--- C++/Practice58.cpp
+++ C++/Practice58.cpp
@@ -233,7 +233,7 @@ void printPattern19(int n) {
         cout << endl;
     }
 
-           int num1 = 8;
+           int num1 = 2*n-2;
         for(int i =1; i<=n; i++) {
 
         //stars 
@@ -254,6 +254,28 @@ void printPattern19(int n) {
         }
 }
 
+void printPattern20(int n) {
+    int spaces = 2*n-2;
+    for(int i =1; i<= 2*n-1; i++) {
+        int stars = i;
+        if(i> n) stars = 2*n-i;
+        //stars
+        for(int j=1; j<= stars;j++) {
+            cout << "*";
+        }
+        //spaces
+        for(int j=1; j<= spaces; j++) {
+            cout << " ";
+        }
+        //stars
+         for(int j=1; j<= stars;j++) {
+            cout << "*";
+        };
+        if(i<n) spaces -=2;
+        else spaces +=2;
+        cout << endl;
+    }
+}
 int main() {
     int t;
     cout << "Enter test case no: ";
@@ -262,6 +284,6 @@ int main() {
     int n;
     cout << "Enter a input number for pattern: ";
     cin >> n;
-    printPattern19(n);
+    printPattern20(n);
     }
 }
